9adcea3fd16681e863e9aed2db966c33fac57122
Oxana <34689920+nialterexova@users.noreply.github.com>
2020-04-09 11:30:07 +0300

    Add files via upload

---
+++ LAB1_ASP/LAB1_ASP1/App_Data/dbo.Employee.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Employee] (
+    [LastName]    VARCHAR (20) NOT NULL,
+    [BirthDate]   DATE     NULL,
+    [TabelNumber] INT          NOT NULL,
+    [PhoneNamber] BIGINT          NOT NULL,
+    [Position]    CHAR (18)    NOT NULL,
+    [email]       CHAR (18)    NULL,
+    [FirstName]   VARCHAR (20) NOT NULL,
+    [MiddleName]  VARCHAR (20) NULL,
+    [Department]  CHAR (18)    NULL,
+    CONSTRAINT [XPKСотрудник] PRIMARY KEY NONCLUSTERED ([TabelNumber] ASC)
+);
+
---
+++ LAB1_ASP/LAB1_ASP1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LAB1_ASP.Global" Language="C#" %>
