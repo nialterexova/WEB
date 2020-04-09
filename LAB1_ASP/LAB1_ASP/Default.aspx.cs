4bd5cc80cc8b7cfa1e09033b114bcb9031596b97
Oxana <34689920+nialterexova@users.noreply.github.com>
2020-04-09 12:40:12 +0300

    1

---
+++ LAB1_ASP/LAB1_ASP/Default.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LAB1_ASP
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /*protected void LinkButton1_Click(object sender, EventArgs e) => Response.Redirect("Browse.aspx?employeeID=pisun");*/
+
+        /*protected void click_1(object sender, EventArgs e)
+        {
+            
+        }*/
+    }
+}
